644b5dd04edcf7218d0883cd95d5c66d927613f1
Maxime CAT <maximecat@outlook.fr>
2023-04-08 10:22:29 +0200

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <img src="/logo-kasa-blanc.png" alt='logo'></img>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
