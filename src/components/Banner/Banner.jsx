07ce1fa98367dc4e21a2102c2ce433dbda428453
Maxime CAT <maximecat@outlook.fr>
2023-04-08 12:45:28 +0200

    Banner modif title

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ function Banner({ title, backgroundUrl }) {
 
     return (
         <section className='banner'>
-            <h1 className='title-banner'>{title}Chez vous, partout et ailleurs</h1>
-            <img className='img-background-banner' src="/background-banner.png" alt="background-banner"></img>
+            <h1 className='title-banner'>{title}</h1>
+            <img className='img-background-banner' src={backgroundUrl = "background-banner.png"} alt="background-banner"></img>
         </section>
     )
 }
