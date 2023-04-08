f017f76fe2bd343241dd0c6e2cd3085ebb356928
Maxime CAT <maximecat@outlook.fr>
2023-04-08 10:22:38 +0200

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <img className='logo-kasa-rouge' src="/logo-kasa-rouge.png" alt='logo'></img>
+            <div className="header-choice">
+                <a className='link-pages' href="/">Accueil</a>
+                <a className='link-pages' href="/about">A Propos</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
