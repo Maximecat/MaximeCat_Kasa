920f00259f6001636f0a5a17a4e25162234debe7
Maxime CAT <maximecat@outlook.fr>
2023-04-09 13:00:10 +0200

    404 ajouts éléments de la page

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,6 +1,12 @@
+import './404.css'
+
 function Error404() {
     return (
-        <h1>404</h1>
+        <div className="Error-page">
+            <h1 className="Error-number">404</h1>
+            <p className="Error-text">Oups! La page que vous demandez n'existe pas.</p>
+            <a className="Error-tohome" href="/">Retourner sur la page d’accueil</a>
+        </div>
     )
 }
 
