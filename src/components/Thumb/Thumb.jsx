d2be94e6f47496f13cc303aaac8a88d62d4a02c5
Maxime CAT <maximecat@outlook.fr>
2023-04-09 12:59:42 +0200

    Thumb ajout dégradé noir

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -2,7 +2,7 @@ import './Thumb.css'
 
 function Thumb({ title, imageUrl }) {
     return (
-        <article style={{ backgroundImage: `url(${imageUrl})` }} className="card" >
+        <article style={{ backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 40%, rgba(0,0,0,0.5) 80%, rgba(0,0,0,1) 100%), url(${imageUrl})` }} className="card" >
             <span className="card-title">{title}</span>
         </article >
     )
