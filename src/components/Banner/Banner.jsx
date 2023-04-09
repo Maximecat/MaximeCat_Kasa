4ab162566bff7d38f21a9064888bea95d12e7dc4
Maxime CAT <maximecat@outlook.fr>
2023-04-09 12:57:54 +0200

    Banner modif link img

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ title, backgroundUrl }) {
     return (
         <section className='banner'>
             <h1 className='title-banner'>{title}</h1>
-            <img className='img-background-banner' src={backgroundUrl = "background-banner.png"} alt="background-banner"></img>
+            <img className='img-background-banner' src={backgroundUrl} alt="background-banner"></img>
         </section>
     )
 }
