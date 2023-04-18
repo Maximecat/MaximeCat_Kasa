d2bcd0b9b33c4fd2b0cdd2b04e2bc165b0329d69
Maxime CAT <maximecat@outlook.fr>
2023-04-18 13:05:00 +0200

    Rating component

---
+++ src/components/Rating/Rating.jsx
@@ -0,0 +1,18 @@
+import './Rating.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+function Rating({ rating, max }) {
+    const maxArray = Array(max).fill(0)
+
+    return (
+        <>
+            {
+                maxArray.map((value, index) =>
+                    <FontAwesomeIcon className={index + 1 <= rating ? "icon-red-star" : "icon-grey-star"} icon={faStar} />)
+            }
+        </>
+    )
+}
+
+export default Rating
