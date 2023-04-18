f77693e221f031b1aeea7de82c38090f0a65c171
Maxime CAT <maximecat@outlook.fr>
2023-04-18 11:47:40 +0200

    Error page

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,11 +1,12 @@
 import './404.css'
+import { Link } from "react-router-dom";
 
 function Error404() {
     return (
         <div className="Error-page">
             <h1 className="Error-number">404</h1>
             <p className="Error-text">Oups! La page que vous demandez n'existe pas.</p>
-            <a className="Error-tohome" href="/">Retourner sur la page d’accueil</a>
+            <Link className="Error-tohome" to="/">Retourner sur la page d’accueil</Link>
         </div>
     )
 }
