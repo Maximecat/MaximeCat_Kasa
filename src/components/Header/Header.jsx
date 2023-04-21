57fb4356248fd412863c7c234983a1a653e03e74
Maxime CAT <maximecat@outlook.fr>
2023-04-21 13:53:47 +0200

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
     return (
         <header>
-            <img className='logo-kasa-rouge' src="/logo-kasa-rouge.png" alt='logo'></img>
+            <img src="/logo-kasa-rouge.png" alt='logo'></img>
             <div className="header-choice">
                 <a className='link-pages' href="/">Accueil</a>
                 <a className='link-pages' href="/about">A Propos</a>
