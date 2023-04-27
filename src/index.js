861340ced48e1c8b2398c97fb1bcfd6896bff559
Maxime CAT <maximecat@outlook.fr>
2023-04-27 11:25:29 +0200

    index js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './pages/Home/App';
 import About from './pages/About/About';
@@ -37,8 +36,3 @@ root.render(
     <Footer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
