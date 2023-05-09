fa3c11dde6fae36a65d78b418511f9778683cf62
Maxime CAT <maximecat@outlook.fr>
2023-05-09 03:16:19 +0200

    index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css';
 import App from './pages/Home/App';
 import About from './pages/About/About';
 import Housing from './pages/Housing/Housing';
+import Error404 from './pages/404/404';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Error404 from './pages/404/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
